4f5208bb0b4c47f07021405e2294fbec72d0b4a5
Martin Norén <martin.noren@sigma.se>
2022-08-29 13:14:46 +0200

    End of section 4

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,34 @@
+using Domain;
+using MediatR;
+using Persistence;
+using static Application.Activities.Create;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+        private readonly DataContext _context;
+
+        public Handler(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+        
+        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        {
+            _context.Activities.Add(request.Activity);
+
+            await _context.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+}
