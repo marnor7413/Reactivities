8491cf597f312c47cb4dd50f9e5a20a085ca98eb
Martin Norén <martin.noren@sigma.se>
2022-08-31 11:43:26 +0200

    Delete on server and btn loader (sect 6)

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,6 +15,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean
 }
 
 export default function ActivityDashboard({ activities,
@@ -25,7 +26,8 @@ export default function ActivityDashboard({ activities,
     openForm,
     closeForm,
     createOrEdit,
-    deleteActivity
+    deleteActivity,
+    submitting
 }: Props) {
     return (
         <Grid>
@@ -33,6 +35,8 @@ export default function ActivityDashboard({ activities,
                 <ActivityList activities={activities} 
                     selectActivity={selectActivity} 
                     deleteActivity={deleteActivity}
+                    submitting={submitting}
+
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -48,6 +52,7 @@ export default function ActivityDashboard({ activities,
                         closeForm={closeForm}
                         activity={selectedActivity}
                         createOrEdit={createOrEdit}
+                        submitting={submitting}
                     />
                 }
 
