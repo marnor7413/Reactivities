ef822f452bd9a7d7cffd8ee00af54c79a8bbe5a0
Martin Norén <martin.noren@sigma.se>
2022-09-15 15:10:11 +0200

    Error handling in API and frontend

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,4 +1,6 @@
+using Application.Core;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 using static Application.Activities.Create;
@@ -7,13 +9,21 @@ namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
     }
 
-    public class Handler : IRequestHandler<Command>
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
+        {
+            RuleFor(x => x.Activity).SetValidator(new ActivityValidator());
+        }
+    }
+
+    public class Handler : IRequestHandler<Command, Result<Unit>>
     {
         private readonly DataContext _context;
 
@@ -21,14 +31,15 @@ namespace Application.Activities
         {
             _context = dataContext;
         }
-        
-        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+
+        public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
         {
             _context.Activities.Add(request.Activity);
+            var result = await _context.SaveChangesAsync() > 0;
 
-            await _context.SaveChangesAsync();
+            if (!result) return Result<Unit>.Failure("Failed to create activity");
 
-            return Unit.Value;
+            return Result<Unit>.Success(Unit.Value);
         }
     }
 }
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,29 +4,38 @@ import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/Home/HomePage';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import ActivityForm from '../../features/activities/Form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
   const location = useLocation();
   return (
     <>
       <Route exact path='/' component={HomePage} />
-      <Route 
+      <Route
         path={'/(.+)'}
         render={() => (
-          <> 
+          <>
+            <ToastContainer position='bottom-right' hideProgressBar />
             <NavBar />
-            <Container style={{marginTop: '7em'}}>
-              <Route exact path='/activities' component={ActivityDashboard} />
-              <Route path='/activities/:id' component={ActivityDetails} />
-              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+            <Container style={{ marginTop: '7em' }}>
+              <Switch>
+                <Route exact path='/activities' component={ActivityDashboard} />
+                <Route path='/activities/:id' component={ActivityDetails} />
+                <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+                <Route path='/errors' component={TestErrors} />
+                <Route path='/server-error' component={ServerError} />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
       />
-      
     </>
   );
 }
