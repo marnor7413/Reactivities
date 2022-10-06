7e2570bb75db559c914d131acf97e8d336ab5869
Martin Norén <martin.noren@sigma.se>
2022-10-06 13:46:39 +0200

    Feature: validate user registration input

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,10 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        
+        /*
+            (?=.*\\d) => at least one char should be number
+            (?=.*[a-z]) => at least one char should be lowercase letter
+            (?=.*[A-Z]) => at least one char should be uppercase letter
+            .{4,8}$ => min 4 chars, max 8 chars
+        */
+        [Required]
+        [RegularExpression("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password must be complex")]
         public string Password { get; set; }
+        
+        [Required]
         public string Username { get; set; }
     }
 }
