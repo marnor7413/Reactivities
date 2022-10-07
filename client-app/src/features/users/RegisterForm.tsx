d38d7f55b9d9a392dd97908a7a4b19c263268e07
Martin Norén <martin.noren@sigma.se>
2022-10-07 15:21:32 +0200

    chore: enhance handling of validationerrors

--- client-app/src/features/users/RegisterForm.tsx
+++ client-app/src/features/users/RegisterForm.tsx
@@ -1,10 +1,11 @@
 import { ErrorMessage, Form, Formik } from 'formik';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import { Button, Header, Label } from 'semantic-ui-react';
+import { Button, Header } from 'semantic-ui-react';
 import MyTextInput from '../../app/Common/Form/MyTextInput';
 import { useStore } from '../../app/stores/store';
 import * as Yup from 'yup';
+import ValidationErrors from '../errors/ValidationErrors';
 
 export default observer(function RegisterForm() {
     const { userStore } = useStore();
@@ -13,16 +14,16 @@ export default observer(function RegisterForm() {
         <Formik
             initialValues={{ displayName: '', username: '', email: '', password: '', error: null }}
             onSubmit={(values, {setErrors}) => userStore.register(values).catch(error => 
-                setErrors({error: 'Invalid email or password'}))}
-                validationSchema={Yup.object({
-                    displayName: Yup.string().required(),
-                    username: Yup.string().required(),
-                    email: Yup.string().required().email(),
-                    password: Yup.string().required(),
-                })}
+                setErrors({error}))}
+            validationSchema={Yup.object({
+                displayName: Yup.string().required(),
+                username: Yup.string().required(),
+                email: Yup.string().required().email(),
+                password: Yup.string().required(),
+            })}
         >
             {({ handleSubmit, isSubmitting, errors, isValid, dirty }) => (
-                <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                <Form className='ui form error' onSubmit={handleSubmit} autoComplete='off'>
                     <Header as='h2' content='Sign up to Reactivities' color='teal' textAlign='center' />
                     <MyTextInput name='displayName' placeholder='Display Name' />
                     <MyTextInput name='username' placeholder='Username' />
@@ -31,10 +32,7 @@ export default observer(function RegisterForm() {
                     <ErrorMessage
                         name='error' 
                         render={() => 
-                            <Label style={{marginBottom: 10}} 
-                                basic 
-                                color='red' 
-                                content={errors.error} />}
+                            <ValidationErrors errors={errors.error}/>}
                     />
                     {/* fluid makes the element fill out the space around itself */}
                     <Button disabled={!isValid || !dirty || isSubmitting } 
