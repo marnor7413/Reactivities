7493bc89acb18335717e35c9a13a3bca6d14f3b9
Martin Norén <martin.noren@sigma.se>
2022-10-07 14:48:23 +0200

    feat: change registerform to modal

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -27,7 +27,6 @@ export default class UserStore {
             
             history.push('/activities');
             store.modalStore.closeModal();
-            console.log(user);
         } catch (error) {
             throw error;
         }
@@ -49,6 +48,21 @@ export default class UserStore {
             runInAction(() => this.user = user);
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    register = async (creds: UserFormValues) => {
+        try {
+            const user = await agent.Account.register(creds);
+            store.commonStore.setToken(user.token);
+            runInAction(() => {
+                this.user = user;
+            });
+            
+            history.push('/activities');
+            store.modalStore.closeModal();
+        } catch (error) {
+            throw error;
         }
     }
 }
